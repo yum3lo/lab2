8ce4265cedef6f0fe726cb836d9173b807586d22
maria <118808220+yum3lo@users.noreply.github.com>
2023-11-19 10:47:03 +0200

    Update ArrayDownStack.java

--- lab3/Stack/ArrayDownStack.java
+++ lab3/Stack/ArrayDownStack.java
@@ -1,48 +1,50 @@
-public class ArrayDown implements Stack {
+public class ArrayDownStack implements Stack {
     private int[] array;
     private int top;
-    private final int MAX_SIZE;
+    private final int MAX_SIZE = 5;
 
-    public ArrayDown(int size) {
-        MAX_SIZE = size;
-        array = new int[size];
-        top = -1;
+    public ArrayDownStack(int MAX_SIZE) {
+        array = new int[MAX_SIZE];
+        top = 4;
     }
 
     @Override
     public void push(int element) {
-        if (isFull()) {
-            System.out.println("Stack is full. Cannot push element.");
-            return;
+        if (!isFull()) {
+            top--;
+            array[top] = element;
+        } else {
+            System.out.println("Stack is full");
         }
-        array[++top] = element - 1;
     }
 
     @Override
     public int pop() {
-        if (isEmpty()) {
-            System.out.println("Stack is empty. Cannot pop element.");
-            return -1; // or throw an exception
+        if (!isEmpty()) {
+            top++;
+            return array[top];
+        } else {
+            System.out.println("Stack is empty");
+            return -1;
         }
-        return array[top--] + 1;
     }
 
     @Override
     public int element() {
         if (isEmpty()) {
-            System.out.println("Stack is empty. No element to return.");
-            return -1; // or throw an exception
+            System.out.println("Stack is empty");
+            return -1;
         }
-        return array[top] + 1;
+        return array[top + 1];
     }
 
     @Override
     public boolean isEmpty() {
-        return top == -1;
+        return top == MAX_SIZE - 1;
     }
 
     @Override
     public boolean isFull() {
-        return top == MAX_SIZE - 1;
+        return top == - 1;
     }
 }
